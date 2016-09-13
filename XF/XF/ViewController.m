a09d3fcf08781030405f684e8950a2e10b26adb5
Frank <37158835@qq.com>
2016-09-13 13:21:41 +0800

    配置工程的基础信息

---
+++ XF/XF/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XF
+//
+//  Created by Frank on 16/9/13.
+//  Copyright © 2016年 free. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
